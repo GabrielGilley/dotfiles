a9ff7753ebb213092c50a79e04ddbce17ddf2b76
Gabriel Gilley <gabriel.gilley22@gmail.com>
2025-04-15 16:15:11 -0500

    set confs

---
+++ install.sh
@@ -0,0 +1,10 @@
+mv ~/.vimrc ~/.legacy_vimrc
+mv ~/.bashrc ~/.legacy_bashrc
+mv ~/.tmux.conf ~/.legacy_tmux.conf
+
+mv vimrc ~/.vimrc
+mv bashrc ~/.bashrc
+mv tmux.conf ~/.tmux.conf
+
+source ~/.bashrc
+
